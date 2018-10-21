6dd45904080053c251ea2aa50835b90920d72fde
itsdevinci <devinbaldwin315@gmail.com>
2018-10-20 21:19:18 -0700

    Updated Projects

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -7,6 +7,11 @@ import Front1 from '../../img/p1_front.png';
 import Back1 from '../../img/p1_back.png';
 import Mobilef1 from '../../img/p1_mobile_front.png';
 import Mobileb1 from '../../img/p1_mobile_back.png';
+import Front2 from '../../img/p2_front.png';
+import Back2 from '../../img/p2_back.png';
+import Mobilef2 from '../../img/p2_mobile_front.png';
+import Mobileb2 from '../../img/p2_mobile_back.png';
+
 
 class Projects extends Component {
 
@@ -26,12 +31,32 @@ class Projects extends Component {
     								</div>
   								</div>
 								</div>
+								<div className="flip-card">
+  								<div className="flip-card-inner">
+    								<div className="flip-card-front">
+										<img className="web" src={Front1} /><img className="mobile" src={Mobilef1} />
+    								</div>
+    								<div className="flip-card-back">
+											<a href="https://www.givememymoney.app"><img className="web" src={Back1} /><img className="mobile" src={Mobileb1} /></a>
+    								</div>
+  								</div>
+								</div>
   							</div>
 								<div className="column">
 								<div className="flip-card">
   								<div className="flip-card-inner">
     								<div className="flip-card-front">
-      								<img className="web" src={Front1} /><img className="mobile" src={Mobilef1} />
+      								<img className="web" src={Front2} /><img className="mobile" src={Mobilef2} />
+    								</div>
+    								<div className="flip-card-back">
+											<a href="#"><img className="web" src={Back2} /><img className="mobile" src={Mobileb2} /></a>
+    								</div>
+  								</div>
+								</div>
+								<div className="flip-card">
+  								<div className="flip-card-inner">
+    								<div className="flip-card-front">
+										<img className="web" src={Front1} /><img className="mobile" src={Mobilef1} />
     								</div>
     								<div className="flip-card-back">
 											<a href="https://www.givememymoney.app"><img className="web" src={Back1} /><img className="mobile" src={Mobileb1} /></a>
@@ -50,8 +75,28 @@ class Projects extends Component {
     								</div>
   								</div>
 								</div>
+								<div className="flip-card">
+  								<div className="flip-card-inner">
+    								<div className="flip-card-front">
+										<img className="web" src={Front1} /><img className="mobile" src={Mobilef1} />
+    								</div>
+    								<div className="flip-card-back">
+											<a href="https://www.givememymoney.app"><img className="web" src={Back1} /><img className="mobile" src={Mobileb1} /></a>
+    								</div>
+  								</div>
+								</div>
   							</div>
 								<div className="column">
+								<div className="flip-card">
+  								<div className="flip-card-inner">
+    								<div className="flip-card-front">
+										<img className="web" src={Front1} /><img className="mobile" src={Mobilef1} />
+    								</div>
+    								<div className="flip-card-back">
+											<a href="https://www.givememymoney.app"><img className="web" src={Back1} /><img className="mobile" src={Mobileb1} /></a>
+    								</div>
+  								</div>
+								</div>
 								<div className="flip-card">
   								<div className="flip-card-inner">
     								<div className="flip-card-front">
