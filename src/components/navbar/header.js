6d42553343a97c395c86afe5149f32223bad1fb0
itsdevinci <devinbaldwin315@gmail.com>
2019-06-05 22:05:26 -0700

    upgrades

--- src/components/navbar/header.js
+++ src/components/navbar/header.js
@@ -1,45 +1,75 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { slide as Menu } from "react-burger-menu";
+import "./navbar.css";
 
 class Header extends Component {
+
+
   render() {
-    const logo = require('../../img/header_letter.png');
+    const logo = require("../../img/header_letter.png");
 
     return (
       //---------HEADER---------//
       <header className="header-main">
-        <Link to='home'>
-        <a className="logo-holder"><img className='logo' src={logo} alt="logo"/></a>
-        </Link>
-      {/* //-------Navigation Start-----// */}
-      <div className="nav">
-        <ul className="main-nav">
-          <Link to='aboutme'>
-            <li>
-              <b>ABOUT</b>
-            </li>
-          </Link>
-          <Link to='projects'>
-            <li>
-              <b>PROJECTS</b>
-            </li>
-          </Link>
-          <Link to='blog'>
-            <li>
-              <b>BLOG</b>
-            </li>
-          </Link>
-          <a href="mailto:devincitechsolutions@gmail.com">
-            <li>
-              <b>CONTACT</b>
-            </li>
+        <Link to="home">
+          <a className="logo-holder">
+            <img className="logo" src={logo} alt="logo" />
           </a>
-        </ul>
-      </div>
+        </Link>
+        {/* //-------Navigation Start-----// */}
+        <div className="nav">
+          <ul className="main-nav">
+            <Link to="aboutme">
+              <li>
+                <b>ABOUT</b>
+              </li>
+            </Link>
+            <Link to="projects">
+              <li>
+                <b>PROJECTS</b>
+              </li>
+            </Link>
+            <Link to="blog">
+              <li>
+                <b>BLOG</b>
+              </li>
+            </Link>
+            <a href="mailto:devincitechsolutions@gmail.com">
+              <li>
+                <b>CONTACT</b>
+              </li>
+            </a>
+          </ul>
+        </div>
+        <Menu id="burger">
+        <ul>
+            <Link to="aboutme">
+              <li className="ham">
+                <b>ABOUT</b>
+              </li>
+            </Link>
+            <Link to="projects">
+              <li className="ham">
+                <b>PROJECTS</b>
+              </li>
+            </Link>
+            <Link to="blog">
+              <li className="ham">
+                <b>BLOG</b>
+              </li>
+            </Link>
+            <a href="mailto:devincitechsolutions@gmail.com">
+              <li className="ham">
+                <b>CONTACT</b>
+              </li>
+            </a>
+          </ul>
+        </Menu>
+
       </header>
-      );
-    }
+    );
+  }
 }
 
 export default Header;
