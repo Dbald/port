8d6e6c4bbbe16885488ba5b92f58ce7b0956aa66
itsdevinci <devinbaldwin315@gmail.com>
2019-11-27 16:38:10 -0800

    small changes

--- src/components/navbar/header.js
+++ src/components/navbar/header.js
@@ -5,14 +5,14 @@ import "./navbar.css";
 
 class Header extends Component {
   state = {
-    isTop: true,
+    isTop: true
   };
 
   componentDidMount() {
-    document.addEventListener('scroll', () => {
+    document.addEventListener("scroll", () => {
       const isTop = window.scrollY < 100;
       if (isTop !== this.state.isTop) {
-          this.setState({ isTop })
+        this.setState({ isTop });
       }
     });
   }
@@ -53,34 +53,6 @@ class Header extends Component {
             </a>
           </ul>
         </div>
-        <Menu>
-        <ul>
-            <li>
-              <img src="https://piskel-imgstore-b.appspot.com/img/116bfed9-8822-11e9-b732-c34339b7f50f.gif" alt="vinci" />
-            </li>
-            <Link to="aboutme">
-              <li className="ham">
-                <b>ABOUT</b>
-              </li>
-            </Link>
-            <Link to="projects">
-              <li className="ham">
-                <b>PROJECTS</b>
-              </li>
-            </Link>
-            <Link to="blog">
-              <li className="ham">
-                <b>BLOG</b>
-              </li>
-            </Link>
-            <a href="mailto:devincitechsolutions@gmail.com">
-              <li className="ham">
-                <b>CONTACT</b>
-              </li>
-            </a>
-          </ul>
-        </Menu>
-
       </header>
     );
   }
